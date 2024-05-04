30b2c1fd5fe14879e145c1ee4dfcd1c22ff60fd3
LeinerViloria <vilorialeiner2002@gmail.com>
2024-05-04 10:59:44 -0500

    Avances

---
+++ Appointment.SDK.Backend/Configuration/StartupExtension.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Appointment.SDK.Backend.Configuration
+{
+    public static class StartupExtension
+    {
+        public static void AddSDKConfiguration(this IServiceCollection services, IConfigurationManager configurationManager)
+        {
+            
+        }
+    }
+}
--- Appointment.SDK.Backend/Database/StoreContext.cs
+++ Appointment.SDK.Backend/Database/StoreContext.cs
@@ -1,9 +1,11 @@
 ﻿
+using Appointment.SDK.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Appointment.SDK.Backend.Database
 {
     public class StoreContext(DbContextOptions options) : DbContext(options)
     {
+        public DbSet<User> Users { get; set; }
     }
 }
