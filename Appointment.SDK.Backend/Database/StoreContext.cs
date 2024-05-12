a273d19d8a0383be5cc83b5853f9965fa68fbf1b
LeinerViloria <vilorialeiner2002@gmail.com>
2024-05-11 19:50:30 -0500

    Ajustes

--- Appointment.SDK.Backend/Database/StoreContext.cs
+++ Appointment.SDK.Backend/Database/StoreContext.cs
@@ -6,7 +6,6 @@ namespace Appointment.SDK.Backend.Database
 {
     public class StoreContext(DbContextOptions options) : DbContext(options)
     {
-        public DbSet<User> Users { get; set; }
         public DbSet<Company> Companies { get; set; }
     }
 }
--- Appointment.SDK.Shared/Entities/BaseUser.cs
+++ Appointment.SDK.Shared/Entities/BaseUser.cs
@@ -5,7 +5,7 @@ using System.ComponentModel.DataAnnotations;
 
 namespace Appointment.SDK.Entities
 {
-    public class User : BaseEntity<int>
+    public abstract class BaseUser<T> : BaseEntity<T>
     {
         [Required]
         [RegularExpression(@"^[^@\s]+@[^@\s]+\.[^@\s]+$", ErrorMessage = "Correo inválido")]
@@ -15,9 +15,6 @@ namespace Appointment.SDK.Entities
         [SensitiveData]
         [StringLength(128, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 14 caracteres.")]
         public string Password { get; set; } = null!;
-
-        [Required]
-        public bool IsAdmin { get; set; }
 
         [Required]
         public EnumRecordStatus Status { get; set; }
