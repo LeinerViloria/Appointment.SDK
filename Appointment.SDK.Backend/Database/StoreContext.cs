2bf79bacb23f0b79b620cd3892da7627056c5a30
LeinerViloria <vilorialeiner2002@gmail.com>
2024-05-19 18:04:29 -0500

    Desaparece BaseCompany

--- Appointment.SDK.Backend/Database/StoreContext.cs
+++ Appointment.SDK.Backend/Database/StoreContext.cs
@@ -1,11 +1,8 @@
 ﻿
-using Appointment.SDK.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Appointment.SDK.Backend.Database
 {
     public class StoreContext(DbContextOptions options) : DbContext(options)
-    {
-        public DbSet<Company> Companies { get; set; }
-    }
+    {}
 }
