74fad359d86360298968f2991eacbbcbcec56381
LeinerViloria <vilorialeiner2002@gmail.com>
2024-05-19 21:01:07 -0500

    Se quita la contraseña con la intención de usar la autenticacion de google

--- Appointment.SDK.Shared/Entities/BaseUser.cs
+++ Appointment.SDK.Shared/Entities/BaseUser.cs
@@ -12,11 +12,6 @@ namespace Appointment.SDK.Entities
         public string Email { get; set; } = null!;
 
         [Required]
-        [SensitiveData]
-        [StringLength(128, MinimumLength = 6, ErrorMessage = "La contraseña debe tener entre 6 y 14 caracteres.")]
-        public string Password { get; set; } = null!;
-
-        [Required]
         public EnumRecordStatus Status { get; set; }
     }
 }
